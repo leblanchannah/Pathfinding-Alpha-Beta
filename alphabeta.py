3e2ea527d3ab4ad55921aa98587b4c324e3a7e6b
Spencer Edwards <13srte@caslab.queensu.ca>
2018-03-11 15:53:07 -0400

    doesn't quite work yet

--- alphabeta.py
+++ alphabeta.py
@@ -1,33 +1,87 @@
 
-
+import math
+import sys
 
 class Graph:
     def __init__(self, nodesInput, edgesInput):
+        self.root = None
         self.nodes = self.parseNodes(nodesInput)     # dictionary where the key is the node (ie. "A"), and the value is "MIN" or "MAX"
-        self.edges = self.parseEdges(edgesInput)     # set of tuples representing edges
+        self.edges = self.parseEdges(edgesInput)     # dictionary of lists representing edges
+        score = self.alphaBeta(self.root, sys.maxsize, -sys.maxsize-1)
+        print(score)
 
     def parseNodes(self, nodesInput):
         nodes = {}
         nodesList = nodesInput.split("),")
+        i = 0
         for node in nodesList:
             temp = node.split(",")
             letter = temp[0].split("(")[1]
             minMax = temp[1].split(")")[0]
             nodes[letter] = minMax
+            if i == 0:
+                self.root = letter
+            i += 1
+        print(nodes)
         return nodes
 
     def parseEdges(self, edgesInput):
-        edges = set()
+        edges = {}
         edgesList = edgesInput.split("),")
         for edge in edgesList:
             temp = edge.split(",")
             node1 = temp[0].split("(")[1]
             node2 = temp[1].split(")")[0]
-            edges.add((node1, node2))
+            edges.setdefault(node1,[]).append(node2)
+        print(edges)
         return edges
 
+    def alphaBeta(self, current, alpha, beta):
+        currentMinMax = self.nodes[current]
+        currentEdges = self.edges[current]
+        print(current)
+        print(currentMinMax)
+        print(currentEdges)
+        isMax = False                       # MIN
+        if currentMinMax == 'MAX':
+            isMax = True
+        hasLeaves = self.hasLeaves(currentEdges)
+        if hasLeaves:
+            leaves = [int(x) for x in currentEdges if x.isdigit() and x != None]
+            if isMax:
+                print(max(leaves))
+                return max(leaves)
+            else:
+                print(min(leaves))
+                return min(leaves)
+        if isMax:
+            for child in currentEdges:
+                value = self.alphaBeta(child, alpha, beta)
+                if value > alpha:
+                    alpha = value
+                if alpha >= beta:
+                    self.edges[current] = []
+                    print(alpha)
+                    return alpha
+            print(alpha)
+            return alpha
+        else:
+            for child in currentEdges:
+                value = self.alphaBeta(child, alpha, beta)
+                if value < beta:
+                    beta = value
+                if alpha >= beta:
+                    self.edges[current] = []
+                    print(beta)
+                    return beta
+            print(beta)
+            return beta
 
-
+    def hasLeaves(self, edges):
+        for child in edges:
+            if child.isdigit():
+                return True
+        return False
 
 def main():
     nodes = "{(A,MAX),(B,MIN),(C,MIN),(D,MAX),(E,MAX),(F,MAX),(G,MAX)}"
