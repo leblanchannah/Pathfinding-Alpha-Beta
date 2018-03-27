f9a72cef05cd959e9399dbfab429d5044263dded
Hannah LeBlanc <leblanchannah95@gmail.com>
2018-03-26 20:13:40 -0400

    pathfinding - changed heapq to a priority queue/python list I maintain myself.

--- pathfinding.py
+++ pathfinding.py
@@ -1,13 +1,8 @@
 ###Pathfinding###
 
-import heapq
 import copy
 from math import sqrt
-
  #Greedy algorithm
-
-
- #A* algorithm  
 
 def solveGrids(inFile, outFile, mode):
     #each grid is separated by an empty line
@@ -26,12 +21,12 @@ def solveGrids(inFile, outFile, mode):
                 out.write("Greedy\n")
                 for i in range(len(soln.greedy)):
                     for j in range(len(soln.greedy[0])):
-                        out.write(soln.greedy[i][j] + " ")
+                        out.write(soln.greedy[i][j] + "")
                     out.write("\n")
                 out.write("A*\n")
                 for i in range(len(soln.astar)):
                     for j in range(len(soln.astar[0])):
-                        out.write(soln.astar[i][j] + " ")
+                        out.write(soln.astar[i][j] + "")
                     out.write("\n")
 
             grid = []
@@ -39,16 +34,27 @@ def solveGrids(inFile, outFile, mode):
             goal = ()
             count = 0
         else:
-            row = list(line)
+            row = list(line) # parse row into a list
             elements = len(row)
             grid.append(row[:elements-1]) #removing end \n char
-            for section in range(elements):
+            for section in range(elements): # for square in row 
                 if row[section] == 'G':
-                    #(row,column)
                     goal = (count,section)
                 elif row[section] == 'S':
                     start = (count,section)
             count += 1
+    if grid != []:
+        soln = GridMap(grid,start,goal,mode)
+        out.write("Greedy\n")
+        for i in range(len(soln.greedy)):
+            for j in range(len(soln.greedy[0])):
+                out.write(soln.greedy[i][j] + "")
+            out.write("\n")
+        out.write("A*\n")
+        for i in range(len(soln.astar)):
+            for j in range(len(soln.astar[0])):
+                out.write(soln.astar[i][j] + "")
+            out.write("\n")
     f.close()
     out.close()
 
@@ -85,13 +91,19 @@ class GridMap:
         for move in temp_path:
             temp_grid[move[0]][move[1]] = 'P'
         return temp_grid
-            
+
     #neighbours determines the possible moves for the search 
     def neighbors(self,current):
         row = current[1][0]
         col = current[1][1]
         around = []
-        if row != 0: #cant move up
+        if col != 0: #left move allowed
+            if self.grid[row][col-1] != 'X':
+                around.append((row,col-1))
+        if col != self.columns: #right move allowed
+            if self.grid[row][col+1] != 'X':
+                around.append((row,col+1))
+        if row != 0: #up move allowed
             if self.grid[row-1][col] != 'X':
                 around.append((row-1,col))
             if self.mode == 'B' and col != 0:
@@ -100,7 +112,7 @@ class GridMap:
             if self.mode == 'B' and col != self.columns:
                 if self.grid[row-1][col+1] != 'X':
                     around.append((row-1,col+1))
-        if row != self.rows:  #cant move down
+        if row != self.rows:  #down move allowed
             if self.grid[row+1][col] != 'X':
                 around.append((row+1,col))
             if self.mode == 'B' and col != 0:
@@ -109,44 +121,37 @@ class GridMap:
             if self.mode == 'B' and col != self.columns:
                 if self.grid[row+1][col+1] != 'X':
                     around.append((row+1,col+1))
-        if col != 0: #cant move left
-            if self.grid[row][col-1] != 'X':
-                around.append((row,col-1))
-        if col != self.columns: #cant move right
-            if self.grid[row][col+1] != 'X':
-                around.append((row,col+1))
+        
         #returns list of tuples (row,col) of legal moves 
         return around
         
     # reconstructs path from search
     def greedyReconstruct(self, cameFrom):
         current = self.goal
-        path = [current]
+        path = []
         while current != self.start:
-            current = cameFrom[current]
             path.append(current)
+            current = cameFrom[current]
+        path.append(self.start)
         path.reverse()
         return path
 
     def greedySearch(self):
-        #priority queue https://docs.python.org/2/library/heapq.html
-        # h = []
-        # heappush(h, (5, 'write code'))
         frontier = []
-        heapq.heappush(frontier, (0,self.start))
+        frontier.insert(0,(0,self.start))
         cameFrom = {}
         cameFrom['S'] = None
-        while not frontier == []:
-            current = heapq.heappop(frontier)
-            heapq.heapify(frontier)
+        while frontier:
+            #heapq.heappop should return the smallest item on the list
+            current = frontier.pop(0)
             if current[1] == self.goal:
                 break
-            neighbors = self.neighbors(current)
-            for next in neighbors:
+            for next in self.neighbors(current):
                 if next not in cameFrom:
-                    priority = self.hFunEuclidean(self.goal,next)
-                    heapq.heappush(frontier, (priority,next))
+                    priority = self.hFunChebyshev(next,self.goal)
+                    frontier.append((priority,next))
                     cameFrom[next] = current[1]
+            frontier = sorted(frontier,key=lambda x:x[0])
         path = self.greedyReconstruct(cameFrom)
         return path
 
@@ -154,26 +159,25 @@ class GridMap:
 
     def aStarSearch(self):
         frontier = []
-        heapq.heappush(frontier, (0,self.start))
+        frontier.insert(0,(0,self.start))
         cameFrom = {}
         csf = {}  #cost so far
         cameFrom['S'] = None
         csf[self.start] = 0
-        while not frontier == []:
-            current = heapq.heappop(frontier)
-            heapq.heapify(frontier)
+        while frontier:
+            current = frontier.pop(0)
             if current[1] == self.goal:
-                break
-            neighbors = self.neighbors(current)
-            for next in neighbors:
+                break 
+            for next in self.neighbors(current):
                 # new Cost = csf[current] + graph.cost(current,next)
                 # all costs are == 1
                 newCost = csf[current[1]] + 1
                 if next not in csf or newCost < csf[next]:
                     csf[next] = newCost
-                    priority = newCost + self.hFunEuclidean(self.goal,next)
-                    heapq.heappush(frontier, (priority,next))
+                    priority = newCost + self.hFunChebyshev(next,self.goal)
+                    frontier.append((priority,next))
                     cameFrom[next] = current[1]
+            frontier = sorted(frontier,key=lambda x:x[0])
         path = self.greedyReconstruct(cameFrom)
         return path
 
@@ -183,12 +187,12 @@ class GridMap:
 ### Heuristic Functions ###
 
     def hFunEuclidean(self, a, b):
-        return sqrt(pow(b[0]-a[0], 2) + pow(b[1] - a[1], 2))
+        return sqrt(pow((b[0]+1)-(a[0]+2), 2) + pow((b[1]+1) - (a[1]+1), 2))
 
-    def hFunManhattan(a, b):
+    def hFunManhattan(self,a, b):
         return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-    def hFunChebyshev(a, b):
+    def hFunChebyshev(self,a, b):
         return max(abs(b[0]-a[0]),abs(b[1]-a[1]))
 
     ### Debugging and Stats ###
